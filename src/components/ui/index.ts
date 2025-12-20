978dca6ee6f83274c9a7f21849be3228f15e915c
juanRCoder <guillecarck@gmail.com>
2025-12-20 17:48:05 -0500

    feat: add new shadcn components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+export { Label } from "./label";
+export { Input } from "./input";
+export { Button } from "./button";
+export { Card, CardHeader, CardTitle, CardContent, CardFooter } from "./card";
